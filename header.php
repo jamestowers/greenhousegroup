de048439407733e1a0b661c03032588d480cb5c8
James Towers <james@songdrop.com>
2016-09-29 15:49:08 +0100

    add favicon

--- header.php
+++ header.php
@@ -21,6 +21,16 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
+
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo bloginfo('template_directory');?>/library/images/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('template_directory');?>/library/images/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('template_directory');?>/library/images/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php echo bloginfo('template_directory');?>/library/images/icons/manifest.json">
+    <link rel="mask-icon" href="<?php echo bloginfo('template_directory');?>/library/images/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo bloginfo('template_directory');?>/library/images/icons/favicon.ico">
+    <meta name="msapplication-config" content="<?php echo bloginfo('template_directory');?>/library/images/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- STANDARD META TAGS -->
     <meta name="description" content="">
     <meta name="author" content="">
